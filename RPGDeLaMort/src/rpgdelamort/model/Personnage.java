9628db86259d46112e5300c9cda6fda2dc184200
meucci97 <stefanomeucci@hotmail.com>
2017-10-04 16:58:21 +0200

    Modif

--- RPGDeLaMort/src/rpgdelamort/model/Personnage.java
+++ RPGDeLaMort/src/rpgdelamort/model/Personnage.java
@@ -2,12 +2,11 @@ package rpgdelamort.model;
 
 import java.util.ArrayList;
 
-
 public class Personnage extends Entite {
 
-	private ArrayList<Arme> arme;
-	private Armure armure;
-	private Classe classe;
+    private ArrayList<Arme> arme;
+    private Armure armure;
+    private Classe classe;
 
     public Armure getArmure() {
         return armure;
@@ -22,6 +21,30 @@ public class Personnage extends Entite {
     }
 
     public void setClasse(Classe classe) {
+        this.classe = classe;
+    }
+
+    public Arme getArmeEquiper() {
+        return arme.get(0);
+    }
+
+    public void setArmeEquiper(Arme a) {
+        arme.add(0, a);
+    }
+
+    //Selection Personnage
+    public Personnage(int id, String nom, float pv, int niveau, double progression, float vitesse, float defense, float force, ArrayList<Arme> arme, Armure armure, Classe classe) {
+        super(id, nom, pv, niveau, progression, vitesse, defense, force);
+        this.arme = arme;
+        this.armure = armure;
+        this.classe = classe;
+    }
+
+    //Creation Personnage
+    public Personnage(String nom, float pv, int niveau, double progression, float vitesse, float defense, float force, ArrayList<Arme> arme, Armure armure, Classe classe) {
+        super(nom, pv, niveau, progression, vitesse, defense, force);
+        this.arme = arme;
+        this.armure = armure;
         this.classe = classe;
     }
 
