6f3b54e3b7764d47b660ad814773c7d0f348b790
meucci97 <stefanomeucci@hotmail.com>
2017-10-04 16:58:10 +0200

    Modif

--- RPGDeLaMort/src/rpgdelamort/model/Entite.java
+++ RPGDeLaMort/src/rpgdelamort/model/Entite.java
@@ -2,13 +2,36 @@ package rpgdelamort.model;
 
 public abstract class Entite {
 
-	protected int id;
-	protected String nom;
-	protected float pv;
-	protected float vitesse;
-	protected int niveau;
-	protected double progression;
-	protected float defense;
-	protected float force;
+    protected int id;
+    protected String nom;
+    protected float pv;
+    protected int niveau;
+    protected double progression;
+    protected float vitesse; //point esquive
+    protected float defense;
+    protected float force;
+
+    //Selection Personnage
+    public Entite(int id, String nom, float pv, int niveau, double progression, float vitesse, float defense, float force) {
+        this.id = id;
+        this.nom = nom;
+        this.pv = pv;
+        this.niveau = niveau;
+        this.progression = progression;
+        this.vitesse = vitesse;
+        this.defense = defense;
+        this.force = force;
+    }
+
+    //Creation Personnage
+    public Entite(String nom, float pv, int niveau, double progression, float vitesse, float defense, float force) {
+        this.nom = nom;
+        this.pv = pv;
+        this.niveau = niveau;
+        this.progression = progression;
+        this.vitesse = vitesse;
+        this.defense = defense;
+        this.force = force;
+    }
 
 }
